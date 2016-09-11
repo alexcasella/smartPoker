74db8d8e6ed4920f4180255983cacfdc4dcd319b
Alex Casella <alex.casella@ibm.com>
2016-09-11 01:08:56 -0400

    complete revamp of cards

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,65 +1,18 @@
-angular.module('cards', []).directive('card', cardDirective);
+function showCards() {
+    var value1 = $("#card1_value").val();
+    var suit1 = $("#card1_suit").val().toLowerCase();
 
-function cardDirective() {
-    return {
-        restrict: 'E',
-        scope: { suit: '@', value: '@' },
-        bindToController: true,
-        controller: cardController,
-        controllerAs: 'card',
-        template: '\n      <corner ng-repeat="corner in card.corners" ng-class="corner">\n        <h1>{{card.display}}</h1>\n        <pip class="{{card.suit.toLowerCase()}}"></p>\n      </corner>\n      <pips>\n        <pip ng-repeat="pip in card.pips" ng-style="{ left: pip.left, top: pip.top }" class="{{card.suit.toLowerCase()}}" ng-class="{ flip: pip.flip }"></pip>\n        <h1 ng-if="!card.pips.length">{{card.display}}</h1>\n      </pips>\n    '
-    };
-}
+    var html = '<div class = "row"><h6>Pre-flop: </h6></div>';
+    html += '<div class = "row">';
+    html += '   <div class="card ' + suit1 + ' v' + value1 +'">';
+    html += '   <div class="top"><div class="number"></div><div class="suit"></div></div><div class="bottom"><div class="number"></div><div class="suit"></div></div></div>';
 
-function cardController($element) {
-    var card = this;
-    this.corners = ['top left', 'bottom right'];
-    this.pips = getPips();
-    this.display = getDisplayValue();
+    var value2 = $("#card2_value").val();
+    var suit2 = $("#card2_suit").val().toLowerCase();
 
-    return init();
+    html += '   <div class="card ' + suit2 + ' v' + value2 +' col-md-offset-1">';
+    html += '   <div class="top"><div class="number"></div><div class="suit"></div></div><div class="bottom"><div class="number"></div><div class="suit"></div></div></div>';
+    html += '</div>';
 
-    function init() {
-        addClasses(card.suit);
-    }
-
-    function addClasses(suit) {
-        $element.addClass(suit === 'Hearts' || suit === 'Diamonds' ? 'red' : 'black');
-    }
-
-    function getDisplayValue() {
-        switch (card.value) {
-            case '10':
-                return '10';
-            default:
-                return card.value.charAt(0);
-        }
-    }
-
-    function getPips() {
-        switch (card.value) {
-            case 'Ace':
-                return [{ left: '50%', top: '50%' }];
-            case '2':
-                return [{ left: '50%', top: '20%' }, { left: '50%', top: '80%', flip: true }];
-            case '3':
-                return [{ left: '50%', top: '50%' }, { left: '50%', top: '20%' }, { left: '50%', top: '80%', flip: true }];
-            case '4':
-                return [{ left: '33%', top: '20%' }, { left: '33%', top: '80%', flip: true }, { left: '67%', top: '20%' }, { left: '67%', top: '80%', flip: true }];
-            case '5':
-                return [{ left: '50%', top: '50%' }, { left: '33%', top: '20%' }, { left: '33%', top: '80%', flip: true }, { left: '67%', top: '20%' }, { left: '67%', top: '80%', flip: true }];
-            case '6':
-                return [{ left: '33%', top: '50%' }, { left: '33%', top: '20%' }, { left: '33%', top: '80%', flip: true }, { left: '67%', top: '50%' }, { left: '67%', top: '20%' }, { left: '67%', top: '80%', flip: true }];
-            case '7':
-                return [{ left: '33%', top: '50%' }, { left: '33%', top: '20%' }, { left: '33%', top: '80%', flip: true }, { left: '67%', top: '50%' }, { left: '67%', top: '20%' }, { left: '67%', top: '80%', flip: true }, { left: '50%', top: '35%' }];
-            case '8':
-                return [{ left: '33%', top: '20%' }, { left: '33%', top: '40%' }, { left: '33%', top: '60%', flip: true }, { left: '33%', top: '80%', flip: true }, { left: '67%', top: '20%' }, { left: '67%', top: '40%' }, { left: '67%', top: '60%', flip: true }, { left: '67%', top: '80%', flip: true }];
-            case '9':
-                return [{ left: '50%', top: '50%' }, { left: '33%', top: '20%' }, { left: '33%', top: '40%' }, { left: '33%', top: '60%', flip: true }, { left: '33%', top: '80%', flip: true }, { left: '67%', top: '20%' }, { left: '67%', top: '40%' }, { left: '67%', top: '60%', flip: true }, { left: '67%', top: '80%', flip: true }];
-            case '10':
-                return [{ left: '50%', top: '35%' }, { left: '50%', top: '65%', flip: true }, { left: '33%', top: '20%' }, { left: '33%', top: '40%' }, { left: '33%', top: '60%', flip: true }, { left: '33%', top: '80%', flip: true }, { left: '67%', top: '20%' }, { left: '67%', top: '40%' }, { left: '67%', top: '60%', flip: true }, { left: '67%', top: '80%', flip: true }];
-            default:
-                return [];
-        }
-    }
+    $("#card_display").append(html);
 }
