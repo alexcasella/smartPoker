22fbc47c9ab2db17bbf5249d454a316fe7bb9045
Alex Casella <alex.casella@ibm.com>
2016-09-14 01:08:14 -0400

    added control panel basics

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ var app = express();
 
 app.use(express.static("public"));
 
-app.get('/', function get() {
+app.get('/', function get(req, res) {
     // res.sendFile(__dirname + '/public/index.html');
     res.sendFile('/index.html');
+});
+
+app.get('/control_panel', function get(req, res) {
+    // res.sendFile(__dirname + '/public/control_panel.html');
+    res.sendFile('/control_panel.html');
 });
 
 app.listen(8000, function(){
